0801ac4b163ed349553749d05a4a001a7ad723c4
Alexander Melo <alexander.melo@ibm.com>
2019-07-01 13:11:38 -0500

    added edit icon

--- src/components/TodoList/Todo/Todo.js
+++ src/components/TodoList/Todo/Todo.js
@@ -2,14 +2,17 @@ import React from 'react';
 import classnames from 'classnames';
 import './Todo.css';
 
-import CheckmarkIcon from '../../../assets/svg/CheckmarkIcon';
+import Icon from '../../../assets/svg/Icon';
 
 const Todo = ({ todo, handleTodoClick }) => {
   return (
     <div className={classnames('Todo', { 'Todo--done': todo.done })}>
-      <div className="Todo-text">{todo.text}</div>
+      <div className="Todo-text">
+        <Icon className="Todo-edit" name="pencil" />
+        <span>{todo.text}</span>
+      </div>
       <div onClick={() => handleTodoClick(todo.id)} className="Todo-checkbox">
-        {todo.done && <CheckmarkIcon />}
+        {todo.done && <Icon name="checkmark" />}
       </div>
     </div>
   );
